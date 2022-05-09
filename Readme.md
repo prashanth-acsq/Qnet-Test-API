ea27fd65e19ed9967d8620fd03e7a98fd7dfe741
prashanth-acsq <prashanth@acsq.com.qa>
2022-05-09 09:35:16 +0300

    Initial

---
+++ Readme.md
@@ -0,0 +1,1 @@
+### **Q-NET REST Feed Test API**
---
+++ main.py
@@ -0,0 +1,34 @@
+import json
+import string
+import random as r
+from fastapi import FastAPI
+
+
+db = json.load(open("db.json", "r"))
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return "Root Page", 
+
+
+@app.get("/random-strings")
+async def random_strings():
+    return "".join(r.choices(string.ascii_letters + ".", k=5)), \
+           "".join(r.choices(string.ascii_letters + ".", k=5)), \
+           "".join(r.choices(string.ascii_letters + ".", k=5))
+
+
+@app.get("/random-number")
+async def random_number():
+    return str(r.randint(100, 10000)), 
+
+
+@app.get("/random-person")
+async def random_person():
+    return str(db["DB"][r.randint(0, len(db["DB"]))]["Name"]), \
+           str(db["DB"][r.randint(0, len(db["DB"]))]["Phone Number"]), \
+           str(db["DB"][r.randint(0, len(db["DB"]))]["Job"])
